1ac251d9f182d5c5e1ed68413c869f89e904f0b8
aleckshen <aleckshn@gmail.com>
2026-08-10 21:24:30 +1200

    try again

--- user/findppid.c
+++ user/findppid.c
@@ -2,9 +2,9 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
-void chain(int n)
+void spawn(int remaining)
 {
-    if (n == 0)
+    if (remaining == 0)
     {
         printf("my ID is %d, my parent ID is %d\n", getpid(), getppid());
         exit(0);
@@ -13,11 +13,14 @@ void chain(int n)
     int pid = fork();
     if (pid == 0)
     {
-        chain(n - 1);
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Winfinite-recursion"
+        spawn(remaining - 1); // child keeps extending the chain
+#pragma GCC diagnostic pop
     }
     else
     {
-        wait(0);
+        wait(0); // wait for child (and everything below it) to finish
         printf("my ID is %d, my parent ID is %d\n", getpid(), getppid());
         exit(0);
     }
@@ -27,12 +30,11 @@ int main(int argc, char *argv[])
 {
     if (argc < 2)
     {
-        printf("usage: findppid [num]\n");
+        fprintf(2, "usage: findppid num\n");
         exit(1);
     }
 
     int num = atoi(argv[1]);
-    chain(num);
-
+    spawn(num);
     exit(0);
 }
